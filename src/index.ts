9b3c4f6d0ec9b40cac96b81ab4f9112d52d9b309
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-07-22 21:48:11 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
+  app.on('*', async context => {
+    context.log({ event: context.event, action: context.payload.action })
+  })
+
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
