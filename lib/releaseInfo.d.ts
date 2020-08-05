23f6ab2cabe94204e4ee7d2675212aae0aea1ccf
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-08-05 21:02:35 +0900

    single file

--- lib/releaseInfo.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class ReleaseInfo {
-    name: string;
-    tag_name: string;
-    body: string;
-    draft: boolean;
-    prerelease: boolean;
-    constructor(name: string, tag_name: string, body: string, draft: boolean, prerelease: boolean);
-}
