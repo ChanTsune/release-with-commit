a274222d4cf4021236ead8ab74d8f4318f686054
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-08-05 23:24:46 +0900

    update all

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
-import { Config } from './config'
-import { getInput } from '@actions/core'
-import { context, GitHub } from '@actions/github'
+import { Config } from "./config";
+import { getInput } from "@actions/core";
+import { context, GitHub } from "@actions/github";
 
 export = async () => {
-  const env = process.env as any
-  const github = new GitHub(env.GITHUB_TOKEN)
-  const {owner, repo} = context.repo
-  const commits = context.payload.commits
+  const env = process.env as any;
+  const github = new GitHub(env.GITHUB_TOKEN);
+  const { owner, repo } = context.repo;
+  const commits = context.payload.commits;
   if (commits.length === 0) {
-    console.log('No commits detected!')
-    return
+    console.log("No commits detected!");
+    return;
   }
   const headCommit = commits[0];
 
   const parsedConfig = Config.parse({
     pushHook: {
-      commitMessageRegExp: getInput('commit_message_regexp'),
-      releaseTitleTemplate: getInput('release_title_template'),
-      releaseTagTemplate: getInput('release_tag_template'),
-      releaseBodyTemplate: getInput('release_body_template'),
-    }
-  })
+      commitMessageRegExp: getInput("commit_message_regexp"),
+      releaseTitleTemplate: getInput("release_title_template"),
+      releaseTagTemplate: getInput("release_tag_template"),
+      releaseBodyTemplate: getInput("release_body_template"),
+    },
+  });
   if (!parsedConfig) {
-    return
+    return;
   }
-  const releaseInfo = parsedConfig.exec(headCommit.message)
+  const releaseInfo = parsedConfig.exec(headCommit.message);
   if (!releaseInfo) {
-    console.log('Commit message does not matched.')
-    return
+    console.log("Commit message does not matched.");
+    return;
   }
 
-  const commitish = context.sha
+  const commitish = context.sha;
   await github.repos.createRelease({
     owner,
     repo,
@@ -39,8 +39,8 @@ export = async () => {
     body: releaseInfo.body,
     draft: releaseInfo.draft,
     prerelease: releaseInfo.prerelease,
-    target_commitish: commitish
-  })
+    target_commitish: commitish,
+  });
 
-  console.log(context)
-}
+  console.log(context);
+};
