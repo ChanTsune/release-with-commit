73478dda50dc4c52244f0c91da2ab75324ff169a
ChanTsune <41658782+ChanTsune@users.noreply.github.com>
2020-08-05 23:30:01 +0900

    Feature/action (#5)
    * Update auto-release.yml
    
    * Revert "Update auto-release.yml"
    
    This reverts commit 45adbfc7be1c55417c9e73697660692f69e6c951.
    
    * update all

--- src/config.ts
+++ src/config.ts
@@ -1,71 +1,68 @@
-import { ReleaseInfo } from './releaseInfo'
+import { ReleaseInfo } from "./releaseInfo";
 
-const arrayLast = (array:Array<string>) => array[array.length - 1]
+const arrayLast = (array: Array<string>) => array[array.length - 1];
 
-const renderTemplate = (r:RegExpExecArray, text:string) => 
-arrayLast(
-  r.map((v, i) => {
-    text = text.replace(`{${i}}`, v)
-    return text
-  })
-)
+const renderTemplate = (r: RegExpExecArray, text: string) =>
+  arrayLast(
+    r.map((v, i) => {
+      text = text.replace(`{${i}}`, v);
+      return text;
+    })
+  );
 
 export class PushHook {
-  private matchResult: RegExpExecArray|null = null
+  private matchResult: RegExpExecArray | null = null;
   constructor(
-      public commitMessageRegExp: RegExp,
-      public releaseTitleTemplate: string,
-      public releaseTagTemplate: string,
-      public releaseBodyTemplate: string,
+    public commitMessageRegExp: RegExp,
+    public releaseTitleTemplate: string,
+    public releaseTagTemplate: string,
+    public releaseBodyTemplate: string
   ) {}
-  exec(commitMessage: string):
-      boolean{this.matchResult = this.commitMessageRegExp.exec(commitMessage)
-    return !!this.matchResult
+  exec(commitMessage: string): boolean {
+    this.matchResult = this.commitMessageRegExp.exec(commitMessage);
+    return !!this.matchResult;
   }
-  get releaseInfo(): ReleaseInfo|null{
-    const r = this.matchResult
+  get releaseInfo(): ReleaseInfo | null {
+    const r = this.matchResult;
     if (r) {
       return new ReleaseInfo(
         renderTemplate(r, this.releaseTitleTemplate),
         renderTemplate(r, this.releaseTagTemplate),
         renderTemplate(r, this.releaseBodyTemplate),
         false,
-        false,
-      )
+        false
+      );
     }
-    return null
+    return null;
   }
   static parse(hook: any): PushHook {
     return new PushHook(
       new RegExp(hook.commitMessageRegExp, "us"),
       hook.releaseTitleTemplate,
       hook.releaseTagTemplate,
-      hook.releaseBodyTemplate,
-    )
+      hook.releaseBodyTemplate
+    );
   }
 }
 
 export class Config {
-  constructor(
-    public pushHook: PushHook
-  ) {}
-  exec(commitMessage:string):ReleaseInfo|null {
-    let result = this.pushHook.exec(commitMessage)
+  constructor(public pushHook: PushHook) {}
+  exec(commitMessage: string): ReleaseInfo | null {
+    const result = this.pushHook.exec(commitMessage);
     if (result) {
-      return this.pushHook.releaseInfo
+      return this.pushHook.releaseInfo;
     }
-    return null
+    return null;
   }
   static parse(config: any): Config {
-      switch (config.version) {
-          default:
-            return Config.parseVersion0(config)
-      }
+    switch (config.version) {
+      default:
+        return Config.parseVersion0(config);
+    }
   }
 
   private static parseVersion0(config: any): Config {
-    const pushHook = PushHook.parse(config)
-    return new Config(pushHook)
+    const pushHook = PushHook.parse(config);
+    return new Config(pushHook);
   }
-
 }
--- test/config.test.ts
+++ test/config.test.ts
@@ -1,31 +1,31 @@
-import { Config, PushHook } from '../src/config'
+import { Config, PushHook } from "../src/config";
 
-describe('Config', () => {
-    let config: any
+describe("Config", () => {
+  let config: any;
 
-    test('Config.parse', async (done) => {
-        const parsedConfig = Config.parse(config)
-        done(expect(parsedConfig.pushHook))
-    })
-    test('PushHook.parse', async (done) => {
-        const pushHook = PushHook.parse(config.pushHook)
-        done(expect(pushHook.releaseBodyTemplate).toBe("{3}"))
-    })
-    test('PushHook.exec', async (done) => {
-        const pushHook = PushHook.parse(config.pushHook)
-        const result = pushHook.exec("Release 1.1.1\n\n- Add\n - function")
-        done(expect(result).toBeTruthy())
-    })
-    test('PushHook.exec', async (done) => {
-        const pushHook = PushHook.parse(config.pushHook)
-        const result = pushHook.exec('Release 1.1.1\n\n- Add\n - function')
-        if (!result) done()
-        const releaseInfo = pushHook.releaseInfo
-        if (releaseInfo) {
-          expect(releaseInfo.name).toBe('Release version 1.1.1')
-          expect(releaseInfo.tag_name).toBe('v1.1.1')
-          expect(releaseInfo.body).toBe('- Add\n - function')
-        }
-        done()
-    })
-})
+  test("Config.parse", async (done) => {
+    const parsedConfig = Config.parse(config);
+    done(expect(parsedConfig.pushHook));
+  });
+  test("PushHook.parse", async (done) => {
+    const pushHook = PushHook.parse(config.pushHook);
+    done(expect(pushHook.releaseBodyTemplate).toBe("{3}"));
+  });
+  test("PushHook.exec", async (done) => {
+    const pushHook = PushHook.parse(config.pushHook);
+    const result = pushHook.exec("Release 1.1.1\n\n- Add\n - function");
+    done(expect(result).toBeTruthy());
+  });
+  test("PushHook.exec", async (done) => {
+    const pushHook = PushHook.parse(config.pushHook);
+    const result = pushHook.exec("Release 1.1.1\n\n- Add\n - function");
+    if (!result) done();
+    const releaseInfo = pushHook.releaseInfo;
+    if (releaseInfo) {
+      expect(releaseInfo.name).toBe("Release version 1.1.1");
+      expect(releaseInfo.tag_name).toBe("v1.1.1");
+      expect(releaseInfo.body).toBe("- Add\n - function");
+    }
+    done();
+  });
+});
