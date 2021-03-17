2b94845fe1dc672337959671d2f63fd83a69ff75
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2021-03-17 15:15:26 +0100

    Had to change test code to let test pass

--- test/config.test.ts
+++ test/config.test.ts
@@ -36,7 +36,7 @@ describe("Config", () => {
     const releaseInfo = config.exec("Release 1.1.1\n\n- Add\n - function");
     if (!releaseInfo) done();
     if (releaseInfo) {
-      expect(releaseInfo.body).toBe("# Release Test md\n\n- Test markdown.\n");
+      expect(releaseInfo.body).toBe("# Release Test md\r\n\r\n- Test markdown.\r\n");
     }
     done();
   });
